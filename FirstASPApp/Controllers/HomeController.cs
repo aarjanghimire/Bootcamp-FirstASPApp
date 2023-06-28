ab31276fc70a361fbf61d0f29dd36f46a8bab0b1
Aarjan Ghimire <aarjanghimire@kathmandu.codes>
2023-06-28 20:13:38 +0545

    Add project files.

---
+++ FirstASPApp/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(int num1, int num2)
+        {
+            int sum = num1 + num2;
+            ViewBag.Sum = sum;
+            return View("Result");
+        }
+    }
+}
